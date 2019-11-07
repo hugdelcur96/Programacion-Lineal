04e537b797a9540e6d1b4e5a438827a3b831e37b
HUGO DELGADO CURIEL <hdelgad4@itam.mx>
2019-11-07 15:50:30 -0600

    Modificaciones al archivo

--- Proyecto I/mSimplexFaseII.m
+++ Proyecto I/mSimplexFaseII.m
@@ -20,21 +20,12 @@ function [x0, z0, ban, iter] = mSimplexFaseII(A, b, c)
     c_simplex = zeros(n + m + 1, 1);
     
     A_simplex(1:m, 1:n) = A;
+    A_simplex(1:m, n+1:n+m) = eye(m);
     b_simplex(1:m) = b;
     c_simplex(1:n) = c;
     
-    
-    for i = 1:m
-        A_simplex(i, n + i) = 1;
-    end
-    
     A_simplex(m + 1, 1:n+m+1) = -c_simplex;
     A_simplex(1:m, n+m+1) = b;
-    
-    
-    x0 = A_simplex;
-    
-    
     
 end
 
