1f10b6251d5139ad59d2ab2e931f763162242921
HUGO DELGADO CURIEL <hdelgad4@itam.mx>
2019-11-07 15:13:51 -0600

    Modificaciones al archivo

--- Proyecto I/mSimplexFaseII.m
+++ Proyecto I/mSimplexFaseII.m
@@ -1,6 +1,22 @@
 function [x0, z0, ban, iter] = mSimplexFaseII(A, b, c)
 
-clear all
-clc
+    %clear all
+    %clc
 
+    [m, n] = size(A);
+    
+    if m ~= size(b)
+        error('El número de renglones de A y la dimensión de b no coinciden.')
+    elseif b < 0
+        error('El vector b tiene que ser mayor o igual que cero.')
+    end
+    
+    A_simplex = zeros(m, n + m);
+    
+    A_simplex(1:m, 1:n) = A;
+
+    for i = 1:m
+        A_simplex(i, n + i) = 1;
+    end
+    
 end
