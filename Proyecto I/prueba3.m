069583e5c96e316c170226964dc2a7c6dc751278
hugdelcur96 <47199487+hugdelcur96@users.noreply.github.com>
2019-11-11 22:39:18 -0600

    Update prueba3.m

--- Proyecto I/prueba3.m
+++ Proyecto I/prueba3.m
@@ -1,29 +1,89 @@
+% Este es el mejor, solo hay que mejorar un poco la notacion y la
+% explicacion.
+
 function [x0, z0, ban, iter] = prueba3(A, b, c)
     
     % Inicializamos la salida
-    x0 = [];
-    z0 = [];
-    ban = 0;
-    iter = 0;
+    x0 = []
+    z0 = []
+    ban = 0
+    iter = 0
     
-    [m, n] = size(A);
+    flagfase2 = 0
+    
+    [m, n] = size(A)
     
     % Conjuntos de índice iniciales
-    N = 1:n;
-    B = n+1:m+n;
+    N = 1:n
+    B = n+1:m+n
     
-    AN = A(:, N);
-    AB = eye(m);
-    invAB = inv(AB);
-    cN = c(N);
-    cB = zeros(1, m);
+    cN = c(N)
+    cB = zeros(1, m)
+    c = [cN cB]
     
-    x0(B) = invAB * b';
-    w = cB * invAB;
-    rN = w * AN - cN;
+    AN = A(:, N)
+    AB = eye(m)
+    A = [AN AB]
+    invAB = inv(AB)
     
+    xB = invAB * b'
+    w = cB * invAB
+    HRN = w * AN
+    sN = cN - HRN
     
-    
+    while flagfase2 == 0
+        if ~all(sN >= 0) == 1
+            [~, t] = min(sN)
+            l = N(t)
+            h_l = invAB * A(:, l)
+            optTest = h_l > 0
+            if sum(optTest) > 0
+                mrt = find(h_l > 0)
+                Xb_hl_div = xB(mrt) ./ h_l(mrt)
+                [p, r] = min(Xb_hl_div)
+                rr = find(Xb_hl_div == p)
+                
+                if length(rr) > 1
+                    r = mrt(rr)
+                    k = B(r)
+                    [k, ~] = max(k)
+                    r = find(B == k)
+                else
+                    r = mrt(r)
+                    k = B(r)
+                end
+                
+                a = sN(t)
+                f = xB(r)
+                B(r) = l
+                g = h_l(r)
+                N(t) = k
+                AN = A(:, N)
+                iter = iter + 1
+                xB(r) = 0
+                h_l2 = h_l
+                h_l2(r) = -1
+                xB = xB - (f / g) * h_l2
+                sN(t) = 0
+                HRN = invAB(r, :) * AN
+                sN = sN - (a / g) * HRN
+                invAB = inv(A(:, B))
+                h_l(r) = g
+            else
+                x0 = {}
+                z0 = {}
+                ban = 1
+                iter = iter + 1
+                return
+            end 
+        else
+            x0 = xB
+            z0 = c(B) * xB
+            ban = 0
+            iter = iter + 1
+            return
+        end
+    end
 end
 
 
