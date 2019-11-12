1f37392f60e1d0a2d3a3ac89caed16dffdb01f20
hugdelcur96 <47199487+hugdelcur96@users.noreply.github.com>
2019-11-12 10:47:51 -0600

    Create prueba4.m

---
+++ Proyecto I/prueba4.m
@@ -0,0 +1,88 @@
+% Una última prueba
+
+function [x0, z0, ban, iter] = prueba4(A, b, c)
+    
+    % Inicializamos la salida
+    x0 = []
+    z0 = []
+    ban = 0
+    iter = 0
+    
+    bandera = 0
+    
+    [m, n] = size(A)
+    
+    % Conjuntos de índice iniciales
+    N = 1:n
+    B = n+1:m+n
+    
+    % Costos básicos y no básicos
+    cN = c(N)
+    cB = zeros(1, m)
+    c = [cN cB]
+    
+    % Matriz básica, no básica y matriz extendida
+    AN = A(:, N)
+    AB = eye(m)
+    A = [AN AB]
+    invAB = inv(AB)         % AB^(-1)
+    
+    xB = invAB * b'         % soluciones básicas
+    lambda = cB * invAB     % variables de holgura
+    HRN = lambda * AN       %
+    rN = HRN - cN           % ahorro no básico
+    
+    while bandera == 0
+        if ~all(rN <= 0) == 1
+            [~, t] = max(rN)
+            l = N(t)
+            h_l = invAB * A(:, l)
+            optTest = h_l > 0
+            if sum(optTest) > 0
+                mrt = find(h_l > 0)
+                Xb_hl_div = xB(mrt) ./ h_l(mrt)
+                [p, r] = min(Xb_hl_div)
+                rr = find(Xb_hl_div == p)
+                
+                if length(rr) > 1
+                    r = mrt(rr)
+                    k = B(r)
+                    [k, ~] = max(k)
+                    r = find(B == k)
+                else
+                    r = mrt(r)
+                    k = B(r)
+                end
+                
+                a = rN(t)
+                f = xB(r)
+                B(r) = l
+                g = h_l(r)
+                N(t) = k
+                AN = A(:, N)
+                iter = iter + 1
+                xB(r) = 0
+                h_l2 = h_l
+                h_l2(r) = -1
+                xB = xB - (f / g) * h_l2
+                rN(t) = 0
+                HRN = invAB(r, :) * AN
+                rN = rN - (a / g) * HRN
+                invAB = inv(A(:, B))
+                h_l(r) = g
+            else
+                x0 = {}
+                z0 = {}
+                ban = 1
+                iter
+                return
+            end 
+        else
+            x0 = xB
+            z0 = c(B) * xB
+            ban = 0
+            iter
+            return
+        end
+    end
+end
